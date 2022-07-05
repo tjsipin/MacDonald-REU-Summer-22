637aa12320f3ee891e8a05575eb01cdfaf47971b
TJ Sipin <tjsipin@esm.ucsb.edu>
2022-07-05 12:27:05 -0700

    reorganized

---
+++ models/xgboost.r
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:537fe2f5359b4c3da62b6604ef8c067892dc3ac7e4e5f9206b10a29044fce65e
+size 7011
--- xgboost.r
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:93555984c309b2859ed82d3472b15eab20f6603f0d420e2e77cc3ea518405e61
-size 7258
