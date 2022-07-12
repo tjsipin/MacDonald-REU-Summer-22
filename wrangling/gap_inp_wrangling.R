a63f88c3e5685f47d03be5f0188c60c59768bd0e
TJ Sipin <tjsipin@gmail.com>
2022-07-12 15:34:09 -0700

    good impute

--- wrangling/gap_inp_wrangling.R
+++ wrangling/gap_inp_wrangling.R
@@ -1,11 +1,11 @@
 data <- gap_inp %>%
-  select(-c(28:67))
+  select(-c('Chikungunya':'Zika',28:67))
 
-data <- cbind(data, aad %>% select(c(1,4,28:69)))
+data <- cbind(data, aad %>% select(c(1,4,'Chikungunya':'Zika',28:69)))
 
 data <- data %>%
   relocate(Code, .after = 1) %>% 
   relocate(Name, .after = 2)
 
-save(data, file = './data/imp.R')
+save(data, file = './data/imp')
 
