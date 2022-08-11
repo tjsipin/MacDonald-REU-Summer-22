49d35db52b7bf2a8f45da470541c09e76e8c1052
Lyndsey Umsted <lyndseyumsted@ucsb.edu>
2022-08-11 12:39:06 -0700

    my extrapolation work

--- models/ARIMA_time_series_model.R
+++ models/ARIMA_time_series_model.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc3155e58562253b1cb1576f357bc6dea63af0b9cd66a281728652a8d5483177
-size 7112
+oid sha256:ef9e8f51dd8b050587bb333b6dce78fea284b0fb21c126003010589e39fd355d
+size 7679
